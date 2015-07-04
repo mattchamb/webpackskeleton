55c31ff410ce55621e09a15b55870a0ac31a4d9f
Matthew Chambers <matthew@chambers.net.nz>
2015-07-04 12:02:36 +1200

    Add nodemon to gulpfile
    Node server is now restarted on code changes.

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,25 @@
 var gulp = require('gulp');
 var ts = require('gulp-typescript');
-var merge = require('merge2');
+var nodemon = require('gulp-nodemon');
 
 var tsProject = ts.createProject({
-    declarationFiles: true,
     module: "commonjs"
 });
 
-gulp.task('scripts', function() {
+gulp.task('typescript', function () {
     var tsResult = gulp.src('server/**/*.ts')
         .pipe(ts(tsProject));
-
-    return merge([ // Merge the two output streams, so this task is finished when the IO of both operations are done. 
-        tsResult.dts.pipe(gulp.dest('build/definitions')),
-        tsResult.js.pipe(gulp.dest('build'))
-    ]);
+    return tsResult.js.pipe(gulp.dest('build'));
 });
 
-gulp.task('watch', ['scripts'], function() {
-    gulp.watch('server/**/*.ts', ['scripts']);
+gulp.task('watch', ['typescript'], function () {
+    gulp.watch('server/**/*.ts', ['typescript']);
+    nodemon({
+        script: './build/server.js',
+        ext: 'js',
+        ignore: ['./build/public/*']
+    })
+        .on('restart', function () {
+            console.log('restarted!')
+        })
 });
