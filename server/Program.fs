6efb141202a9dfc33691432750d5929db4a5c8e0
Matthew Chambers <matthew@chambers.net.nz>
2015-07-05 15:26:21 +1200

    Add Suave.io webserver to serve up the static files

--- server/Program.fs
+++ server/Program.fs
@@ -1,7 +1,35 @@
-﻿// Learn more about F# at http://fsharp.net
-// See the 'F# Tutorial' project for more help.
+﻿open System
+open System.IO
+
+open Suave
+open Suave.Http
+open Suave.Http.Applicatives
+open Suave.Http.Successful
+open Suave.Http.RequestErrors
+open Suave.Web
+open Suave.Types
+
+
+let serveFiles = 
+    GET >>= pathRegex ".*"
+        >>= choose [
+            // Try serving the requested file
+            request(fun r -> Files.browseFileHome r.url.LocalPath);
+            // Otherwise serve index.html so that the server works with html5 history api
+            request(fun r -> Files.browseFileHome "index.html");
+        ]
+
+let app = 
+    choose [
+        path "/api/hello" 
+            >>= choose [ GET >>= OK "Hello from F#"
+                         NOT_FOUND "Found no handlers" ]
+        serveFiles
+        NOT_FOUND "Found no handlers"
+    ]
 
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
-    0 // return an integer exit code
+    let contentPath = Some (Path.Combine(Environment.CurrentDirectory, "public"))
+    startWebServer {defaultConfig with homeFolder = contentPath} app
+    0
