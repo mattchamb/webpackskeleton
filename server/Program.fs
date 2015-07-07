7b7187d582a3483367dd53b3cb34e8e1e1ab512c
Matthew Chambers <matthew@chambers.net.nz>
2015-07-08 00:16:26 +1200

    Format code

--- server/Program.fs
+++ server/Program.fs
@@ -1,8 +1,6 @@
 ﻿open System
 open System.IO
-
 open EdgeJs
-
 open Suave
 open Suave.Logging
 open Suave.Http
@@ -14,101 +12,86 @@ open Suave.Types
 open System.Net
 open Suave.Razor
 
-type StaticContent =
-    {scripts: string array;
-     styles: string array}
+type StaticContent = 
+    { scripts : string array
+      styles : string array }
 
 type TemplateModel = 
-    {prerenderedContent: string;
-     staticContent: StaticContent}
+    { prerenderedContent : string
+      staticContent : StaticContent }
 
 let contentLocation =
-#if DEBUG
+#if DEBUG 
     "http://localhost:9090/"
 #else
     ""
 #endif
+    
 
 let defaultModel = 
-    {
-        prerenderedContent = "";
-        staticContent = 
-        {
-            scripts = 
-                [|
-                    contentLocation + "commons.js";
-                    contentLocation + "main.js"
-                |];
-            styles = 
-                [|contentLocation + "main.css"|]
-        }
-    }
+    { prerenderedContent = ""
+      staticContent = 
+          { scripts = 
+                [| contentLocation + "commons.js"
+                   contentLocation + "main.js" |]
+            styles = [| contentLocation + "main.css" |] } }
 
 let indexTemplate = razor<TemplateModel> "Index.cshtml"
 
-let useEntryPoint (url: System.Uri) =
+let useEntryPoint (url : System.Uri) = 
     match url.LocalPath with
     | "/" -> indexTemplate defaultModel
     | _ -> indexTemplate defaultModel
 
-let jsFunc<'TArg, 'TResult> code (arg: 'TArg) =
-    async {
+let jsFunc<'TArg, 'TResult> code (arg : 'TArg) = 
+    async { 
         let func = Edge.Func code
         let boxedArg = box arg
         let! result = Async.AwaitTask <| func.Invoke boxedArg
         return result :?> 'TResult
     }
 
-let test: int -> Async<int> = jsFunc "return function(x, cb) { cb(null, x * 2); }"
+let test : int -> Async<int> = jsFunc "return function(x, cb) { cb(null, x * 2); }"
 
 let serveFiles = 
-    GET >>= pathRegex ".*"
-        >>= choose [
-            // Try serving the requested file
-            request(fun r -> Files.browseFile (Path.Combine(Environment.CurrentDirectory, "public")) r.url.LocalPath);
-            path "/favicon.ico" >>= NOT_FOUND "File not found.";
-            // Otherwise serve index.html so that the server works with html5 history api
-            request(fun r -> useEntryPoint r.url);
-        ]
+    GET >>= pathRegex ".*" >>= choose [ // Try serving the requested file
+                                        request 
+                                            (fun r -> 
+                                            Files.browseFile (Path.Combine(Environment.CurrentDirectory, "public")) 
+                                                r.url.LocalPath)
+                                        path "/favicon.ico" >>= NOT_FOUND "File not found."
+                                        // Otherwise serve index.html so that the server works with html5 history api
+                                        request (fun r -> useEntryPoint r.url) ]
 
 let app = 
-    choose [
-        path "/api/hello" 
-            >>= GET >>= (fun x -> async {
-                let! r = test 5;
-                return! OK (sprintf "The result is: %A" r) x
-            });
-        serveFiles;
-        NOT_FOUND "Found no handlers"
-    ]
+    choose [ path "/api/hello" >>= GET >>= (fun x -> async { let! r = test 5
+                                                             return! OK (sprintf "The result is: %A" r) x })
+             serveFiles
+             NOT_FOUND "Found no handlers" ]
 
 [<EntryPoint>]
 let main argv = 
     printfn "Command line arguments: %A" argv
-
     let port = 
-        if argv.Length >= 1 then
+        if argv.Length >= 1 then 
             printfn "Using port \"%s\"" argv.[0]
             let p, a = UInt16.TryParse argv.[0]
             if p then a
-            else
-                printfn "Unable to parse port \"%s\", defaulting to port 3000"  argv.[0]
+            else 
+                printfn "Unable to parse port \"%s\", defaulting to port 3000" argv.[0]
                 3000us
         else 
             printfn "Defaulting to port 3000"
             3000us
-
+    
     let absoluteContentPath = 
-        if argv.Length >= 2 then
-            Some argv.[1]
-        else
-            Some Environment.CurrentDirectory
-
-    let config = { 
-        defaultConfig with
-            homeFolder = absoluteContentPath;
-            bindings = [ HttpBinding.mk HTTP IPAddress.Loopback port];
-            logger = Loggers.ConsoleWindowLogger (minLevel=LogLevel.Verbose, colourise=true)
-    }
-    startWebServer config  app
+        if argv.Length >= 2 then Some argv.[1]
+        else Some Environment.CurrentDirectory
+    
+    let config = 
+        { defaultConfig with homeFolder = absoluteContentPath
+                             bindings = [ HttpBinding.mk HTTP IPAddress.Loopback port ]
+                             logger = Loggers.ConsoleWindowLogger(minLevel = LogLevel.Verbose, colourise = true) }
+    
+    startWebServer config app
     0
